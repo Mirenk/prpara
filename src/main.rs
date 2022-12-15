4a5ea1afee68cd489a15118f0dddb55725297a33
mirenk <minikmwinsea@gmail.com>
2022-12-16 01:36:45 +0900

    [update] impl arg parse

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
+use seahorse::{App, Context, Flag, FlagType};
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    let app = App::new(env!("CARGO_PKG_NAME"))
+        .description(env!("CARGO_PKG_DESCRIPTION"))
+        .author(env!("CARGO_PKG_AUTHORS"))
+        .version(env!("CARGO_PKG_VERSION"))
+        .usage("cli --pid [pid]")
+        .action(print_arg)
+        .flag(
+            Flag::new("pid", FlagType::Int)
+            .description("pid")
+            .alias("p"),
+            );
+
+    app.run(args);
+}
+
+fn usage(c: &Context) {
+    println!("Usage: cli --pid [pid]");
+}
+
+fn print_arg(c: &Context) {
+    if let Ok(pid) = c.int_flag("pid"){
+        println!("{}", pid);
+    } else {
+        usage(c);
+    }
 }
