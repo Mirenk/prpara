9099aee9d7eaf2e22e3cb2ee712d41b68c30a45e
mirenk <minikmwinsea@gmail.com>
2023-01-20 17:17:38 +0900

    [add] impl mmap core/syscall.rs

--- src/core/syscall.rs
+++ src/core/syscall.rs
@@ -1,3 +1,23 @@
+use nix::libc::{SYS_mmap, MAP_ANONYMOUS, MAP_PRIVATE, PROT_EXEC, PROT_READ, PROT_WRITE};
+
+use super::Proc;
+use crate::Result;
+
+unsafe fn mmap(mut proc: Proc) -> Result<u64> {
+    let mut regs = proc.get_regs()?.clone();
+
+    regs.rax = SYS_mmap as u64;
+    regs.rdx = 0;
+    regs.rsi = 4096;
+    regs.rdx = (PROT_EXEC | PROT_READ | PROT_WRITE) as u64;
+    regs.rcx = (MAP_PRIVATE | MAP_ANONYMOUS) as u64;
+    regs.r10 = regs.rcx;
+    regs.r8 = u64::MAX;
+    regs.r9 = 0;
+
+    return proc.run_syscall(regs);
+}
+
 /*
 pub fn mmap (
     proc: Proc,
