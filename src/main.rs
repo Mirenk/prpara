ef4b1a282e3595bc2662f8f9aa704db1fab12d2e
mirenk <minikmwinsea@gmail.com>
2023-01-28 22:21:42 +0900

    [wip][update] add main args

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,3 @@
-use prpara::core::jmp;
 use prpara::core::load;
 use prpara::core::Proc;
 use seahorse::{App, Context, Flag, FlagType};
@@ -16,26 +15,36 @@ fn main() {
             Flag::new("pid", FlagType::Int)
                 .description("pid")
                 .alias("p"),
+        )
+        .flag(
+            Flag::new("load", FlagType::String)
+                .description("load object file path")
+                .alias("l"),
+        )
+        .flag(
+            Flag::new("func", FlagType::String)
+                .description("update function name")
+                .alias("f"),
         );
 
     app.run(args);
 }
 
 fn usage() {
-    println!("Usage: cli --pid [pid]");
+    println!("Usage: cli --pid [pid] --load [filepath] --func [function name]");
 }
 
 fn run(c: &Context) {
-    if let Ok(pid) = c.int_flag("pid") {
-        if pid > 0 {
-            let pid = pid as u64;
-            let proc = Proc::new(pid).unwrap();
-            load(proc);
+    let Ok(pid) = c.int_flag("pid") else { usage(); return; };
+    let Ok(load_path) = c.string_flag("load") else { usage(); return; };
+    let Ok(func) = c.string_flag("func") else { usage(); return; };
+
+    if pid > 0 {
+        let pid = pid as u64;
+        let proc = Proc::new(pid).unwrap();
+        load(proc);
         //            jmp(proc);
-        } else {
-            eprintln!("error: pid must positive number.");
-        }
     } else {
-        usage();
+        eprintln!("error: pid must positive number.");
     }
 }
