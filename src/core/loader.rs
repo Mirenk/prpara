443cce18bce5eda28f6004f851f06442250ae2b1
mirenk <minikmwinsea@gmail.com>
2023-03-01 21:15:10 +0900

    [wip][refactor] add loader function

--- src/core/loader.rs
+++ src/core/loader.rs
@@ -1,4 +1,4 @@
-use std::collections::HashMap;
+use std::{collections::HashMap, path::Path};
 
 use crate::{
     types::{Address, Pid},
@@ -11,9 +11,21 @@ pub struct Loader {
     proc_sym_map: SymMap,
 }
 
+impl Loader {
+    pub fn get_reloc_object(&self, path: &Path) {}
+}
+
 pub fn new(pid: Pid) -> Result<Loader> {
     let obj = Loader {
         proc_sym_map: SymMap::new(),
     };
     Ok(obj)
+}
+
+fn prpare_proc_sym_map(pid: Pid, proc_sym_map: &mut SymMap) -> Result<()> {
+    Ok(())
+}
+
+pub fn load_syms(path: &Path, sym_map: &mut SymMap) -> Result<()> {
+    Ok(())
 }
