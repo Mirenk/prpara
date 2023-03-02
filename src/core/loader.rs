26c618065771c69a6495e2875395179fdf1dd451
mirenk <minikmwinsea@gmail.com>
2023-03-02 13:36:33 +0900

    [wip][refactor] impl load_syms

--- src/core/loader.rs
+++ src/core/loader.rs
@@ -1,7 +1,9 @@
-use std::{collections::HashMap, path::Path};
+use std::{collections::HashMap, fs, path::Path};
+
+use goblin::Object;
 
 use crate::{
-    types::{Address, Pid},
+    types::{Address, Error, Pid},
     Result,
 };
 
@@ -26,6 +28,30 @@ fn prpare_proc_sym_map(pid: Pid, proc_sym_map: &mut SymMap) -> Result<()> {
     Ok(())
 }
 
-pub fn load_syms(path: &Path, sym_map: &mut SymMap) -> Result<()> {
-    Ok(())
+// load symbol address to HashMap
+pub fn load_syms(path: &Path, sym_map: &mut SymMap, offset: Address) -> Result<()> {
+    let buffer = fs::read(path).map_err(|_| Error::MapError)?;
+    println!("object path: {}", path.display());
+
+    if let Ok(Object::Elf(elf)) = Object::parse(&buffer) {
+        // syms section
+        for sym in elf.syms.iter() {
+            sym_map.insert(
+                elf.strtab.get_at(sym.st_name).unwrap().to_string(), // symbol name
+                sym.st_value + offset,                               // absolute address
+            );
+        }
+
+        // dynsyms section
+        for sym in elf.dynsyms.iter() {
+            sym_map.insert(
+                elf.dynstrtab.get_at(sym.st_name).unwrap().to_string(), // symbol name
+                sym.st_value + offset,                                  // absolute address
+            );
+        }
+
+        Ok(())
+    } else {
+        Err(Error::ElfLoadError)
+    }
 }
