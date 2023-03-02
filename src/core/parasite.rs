8929e0d75a4fa908aed054fa5e19fd6ad20c801e
mirenk <minikmwinsea@gmail.com>
2023-03-02 18:07:21 +0900

    [wip][refactor] impl mem_alloc

--- src/core/parasite.rs
+++ src/core/parasite.rs
@@ -1,3 +1,4 @@
+use nix::libc::{SYS_mmap, MAP_ANONYMOUS, MAP_PRIVATE, PROT_EXEC, PROT_READ, PROT_WRITE};
 use nix::sys::ptrace::{self, Options};
 use nix::sys::signal::Signal;
 use nix::sys::wait::waitpid;
@@ -15,7 +16,19 @@ pub struct Proc {
 
 impl Proc {
     pub fn mem_alloc(&mut self, size: usize) -> Result<Address> {
-        Ok(0)
+        let orig_regs = ptrace::getregs(self.pid).map_err(|_| Error::PtraceGetRegsError)?;
+
+        // mmap(0, size, PROT_EXEC|PROT_READ|PROT_WRITE, MAP_PRIVATE|MAP_ANONYMOUS, -1, 0);
+        let mmap_regs = prepare_mmap(
+            &orig_regs,
+            0,
+            size as u64,
+            (PROT_EXEC | PROT_READ | PROT_WRITE) as u64,
+            (MAP_PRIVATE | MAP_ANONYMOUS) as u64,
+            u64::MAX,
+            0,
+        );
+        self.run_syscall(mmap_regs, None)
     }
     pub fn write_buf(&mut self, addr: Address, data: Vec<u8>) -> Result<()> {
         Ok(())
@@ -50,7 +63,27 @@ pub fn new(pid: Pid) -> Result<Proc> {
     }
 }
 
-fn prepare_mmap(orig_regs: &user_regs_struct) -> user_regs_struct {
+// example for run_syscall()
+fn prepare_mmap(
+    orig_regs: &user_regs_struct,
+    addr: u64,
+    len: u64,
+    prot: u64,
+    flags: u64,
+    fd: u64,
+    offset: u64,
+) -> user_regs_struct {
     let mut regs = orig_regs.clone();
+
+    // set args to regs
+    regs.rax = SYS_mmap as u64;
+    regs.rdx = addr;
+    regs.rsi = len;
+    regs.rdi = prot;
+    regs.rcx = flags;
+    regs.r10 = flags;
+    regs.r8 = fd;
+    regs.r9 = offset;
+
     regs
 }
