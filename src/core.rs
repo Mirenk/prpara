3beae8a39bd15f57505e42e3de19dd5f809b824f
mirenk <minikmwinsea@gmail.com>
2023-03-02 13:23:04 +0900

    [wip][refactor] add core function

--- src/core.rs
+++ src/core.rs
@@ -1,3 +1,5 @@
+use std::path::Path;
+
 use crate::{types::Pid, Result};
 
 pub mod loader;
@@ -6,6 +8,17 @@ pub mod parasite;
 pub struct Target {
     proc: parasite::Proc,
     loader: loader::Loader,
+}
+
+impl Target {
+    pub fn parasite_func(
+        &self,
+        object_path: &Path,
+        old_func_sym_name: String,
+        new_func_sym_name: String,
+    ) -> Result<()> {
+        Ok(())
+    }
 }
 
 pub fn new(pid: Pid) -> Result<Target> {
